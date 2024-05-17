4af8e2ba247aa86e0f2db6bf54efcff71605023f
Aamish Hussain <aamishhussain23@gmail.com>
2024-05-17 09:49:51 +0530

    added CORS

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import requests
 
 app = FastAPI()
+
+# Set all CORS enabled origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Payload(BaseModel):
     token: str
